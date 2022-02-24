8695719d650b0b6691a3cb422babb40ebf06e434
dimasgit <aansaputra8890@gmail.com>
2022-02-24 13:28:32 +0700

    fixing navbar style

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,14 +1,30 @@
 <template>
-<div class="card w-96 glass">
-  <figure><img src="https://api.lorem.space/image/movie?w=400&h=225" alt="car!"></figure>
-  <div class="card-body">
-    <h2 class="card-title">New Movies</h2>
-    <p>Buy our ticket now</p>
-    <div class="justify-end card-actions">
-      <button class="btn btn-primary">Watch Now</button>
+  <div class="flex align-center justify-center w-10/12 py-auto mx-auto">
+    <div class="card w-96 bg-base-100 card-compact shadow-xl">
+      <figure>
+        <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes">
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="justify-end card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    <div class="card w-96 bg-base-100 card-compact shadow-xl">
+      <figure>
+        <img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes">
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="justify-end card-actions">
+          <button class="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,31 @@
 <template>
-  <div class="navbar bg-base-100">
-      <h2 class="btn btn-ghost normal-case text-3xl">Movue</h2>
-      <div class="navbar-end">
+  <div class="navbar bg-base-100s">
+      <h2 class="btn btn-ghost normal-case text-4xl">Movue</h2>
+      <div class="navbar-end w-10/12">
         <ul
-        class="menu flex-none justify-between bg-base-100 w-4/12 menu-horizontal sm p-0 rounded-box">
-          <li><a>Home</a></li>
+        class="flex sm:hidden w-8/12 md:w-10/12 flex-none items-center justify-around bg-base-100 p-6 rounded-box">
+          <li><a href="">Home</a></li>
           <li class="dropdown dropdown-hover">
-            <a class="m-1 btn" tabindex="0">Genres</a>
-              <ul tabindex="0" class="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
-                <li><a>Action</a></li>
-                <li><a>Adventures</a></li>
-                <li><a>Comedy</a></li>
-                <li><a>Drama</a></li>
-                <li><a>Fantasy</a></li>
-                <li><a>Horror</a></li>
-                <li><a>Romance</a></li>
-                <li><a>Sci-Fi</a></li>
-                <li><a>Thriller</a></li>
+            <a href="">Genres</a>
+              <ul class="shadow menu dropdown-content bg-base-100 rounded-box w-56">
+                <li><a href="">Action</a></li>
+                <li><a href="">Adventures</a></li>
+                <li><a href="">Comedy</a></li>
+                <li><a href="">Drama</a></li>
+                <li><a href="">Fantasy</a></li>
+                <li><a href="">Horror</a></li>
+                <li><a href="">Romance</a></li>
+                <li><a href="">Sci-Fi</a></li>
+                <li><a href="">Thriller</a></li>
               </ul>
           </li>
-          <li><a>Movies</a></li>
-          <li><a>Anime</a></li>
+          <li><a href="">Movies</a></li>
+          <li><a href="">Anime</a></li>
         </ul>
-
+        <div class="hamburger cursor-pointer hidden sm:flex mx-5">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
+          </svg>
+        </div>
       </div>
   </div>
 </template>
